0075333a7d4969b66d2814e70ce4a7b1431b2727
TheJongu <60642840+TheJongu@users.noreply.github.com>
2021-06-12 10:51:58 +0200

    Added Robot Movement

---
+++ 2021_GameJam_Robot/Assets/Objects/Playable/Robot_Small_Movement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Robot_Small_Movement : MonoBehaviour
+{
+    // Keycodes, defines the key for movement
+    public KeyCode keyCode_up;
+    public KeyCode keyCode_down;
+    public KeyCode keyCode_left;
+    public KeyCode keyCode_right;
+
+    private bool isMoving;
+    private Vector3 orgPos, targetPos;
+    private float timePerMove = 0.2f;
+    // How many moves are still possible?
+    private int charge = 100;
+
+    private IEnumerable coroutine;
+
+    public LayerMask collisionMask;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(keyCode_up) && !isMoving && charge > 0){
+            StartCoroutine("MovePlayer",Vector3.up);
+        }
+        if(Input.GetKey(keyCode_down) && !isMoving && charge > 0){
+            StartCoroutine("MovePlayer",Vector3.down);
+        }
+        if(Input.GetKey(keyCode_left) && !isMoving && charge > 0){
+            StartCoroutine("MovePlayer",Vector3.left);
+        }
+        if(Input.GetKey(keyCode_right) && !isMoving && charge > 0){
+            StartCoroutine("MovePlayer",Vector3.right);
+        }
+    }
+    private IEnumerator MovePlayer(Vector3 aDirection){
+        isMoving = true;
+        float timePassed = 0;
+        orgPos = transform.position;
+        targetPos = orgPos + aDirection;
+
+        while(timePassed < timePerMove){
+            transform.position = Vector3.Lerp(orgPos, targetPos, (timePassed / timePerMove));
+            timePassed += Time.deltaTime;
+            yield return null;
+        }
+        // At the end of the Movement, set Target to be exact
+        transform.position = targetPos;
+        charge--;
+
+        isMoving = false;
+    }
+}
